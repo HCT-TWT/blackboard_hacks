7696c32402dab3837817ad3a800a24c6807c0b24
Raghad-Nih <r.nihlawi@gmail.com>
2020-07-12 14:16:22 +0400

    Update reviewStatusGeneric.js

--- generic/reviewStatusGeneric/reviewStatusGeneric.js
+++ generic/reviewStatusGeneric/reviewStatusGeneric.js
@@ -2,7 +2,7 @@
     /* Insert CSS to style search input */
     var link = document.createElement("link");
     link.setAttribute("rel", "stylesheet");
-    link.setAttribute("href", "https://humsstel.github.io/blackboard_hacks/generic/reviewStatusGeneric/reviewStatusGeneric.css");
+    link.setAttribute("href", "https://raghad-nih.github.io/reviewStatus.css");
     var head = document.getElementsByTagName("head") [0];
     head.appendChild(link);
     /* Insert Font Awesome for search icon */
