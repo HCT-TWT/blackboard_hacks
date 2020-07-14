5dbccd83b64db1e3cbbc2162c09411d373df262a
HCT-TWT <68266371+HCT-TWT@users.noreply.github.com>
2020-07-14 09:59:18 +0400

    Update reviewStatus1.js

--- generic/reviewStatusGeneric/reviewStatus1.js
+++ generic/reviewStatusGeneric/reviewStatus1.js
@@ -2,7 +2,7 @@
     /* Insert CSS to style search input */
     var link = document.createElement("link");
     link.setAttribute("rel", "stylesheet");
-    link.setAttribute("href", "https://raghad-nih.github.io/reviewStatus1.css");
+    link.setAttribute("href", "https://hct-twt.github.io/reviewStatus1.css");
     var head = document.getElementsByTagName("head") [0];
     head.appendChild(link);
     /* Insert Font Awesome for search icon */
